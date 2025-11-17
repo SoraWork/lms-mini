3461b83e1d09f6787a3fe39aa60505c02b42f861
HoaiPhong <phongnguyenhoai2003@gmail.com>
2025-11-17 09:50:09 +0700

    create project

---
+++ src/main/java/com/hoaiphong/lmsmini/LmsMiniApplication.java
@@ -0,0 +1,13 @@
+package com.hoaiphong.lmsmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsMiniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsMiniApplication.class, args);
+    }
+
+}
